121b3959b28031d295f6e9423ec6e4ac71b6791d
Eric Sink <eric@sourcegear.com>
2022-06-12 00:04:28 -0500

    comments

--- Sources/WinRT/Object.swift
+++ Sources/WinRT/Object.swift
@@ -5,6 +5,12 @@ open class Object
     internal init(plok: WinRT.IInspectable?) throws {
         _self = plok!
     }
+    // TODO this deliberately has a different name from Interface(),
+    // which all the derived clases use, because giving this the
+    // same name triggers a ton of errors about needing an override
+    // keyword, and when we add that keyword, we get errors saying
+    // it isn't needed.  probably because all the subclasses return
+    // something that inherits from the type this one returns.
     internal final func GetInterface() throws -> WinRT.IInspectable {
         return _self
     }
