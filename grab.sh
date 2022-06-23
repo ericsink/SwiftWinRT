d61b25741f7ab22ec21efdb1e65d0e5c5173817e
Eric Sink <eric@sourcegear.com>
2022-06-23 15:17:07 -0500

    mv all the auto-generated files down into a subdir called generated

--- grab.sh
+++ grab.sh
@@ -3,15 +3,15 @@ cp ../bridge/pkg/sourcegear-bridge-WinRT/C/WinRT/include/WinRT.h ./WindowsSdk/So
 cp ../bridge/pkg/sourcegear-bridge-WinRT/C/WinRT/include/WinRT.h ./WindowsApp/Sources/CWindowsApp/include/WindowsApp.h
 cp ../bridge/pkg/sourcegear-bridge-WinRT/C/WinRT/include/WinRT.h ./Win2D/Sources/CWin2D/include/Win2D.h
 
-rm ./WindowsSdk/Sources/WindowsSdk/*.swift
-cp ../bridge/pkg/sourcegear-bridge-WinRT/Sources/WinRT/*.swift ./WindowsSdk/Sources/WindowsSdk/
+rm ./WindowsSdk/Sources/WindowsSdk/generated/*.swift
+cp ../bridge/pkg/sourcegear-bridge-WinRT/Sources/WinRT/*.swift ./WindowsSdk/Sources/WindowsSdk/generated
 
 # TODO this is cheesy
 
-rm ./WindowsApp/Sources/WindowsApp/*.swift
-mv ./WindowsSdk/Sources/WindowsSdk/Microsoft.*.swift ./WindowsApp/Sources/WindowsApp/
-mv ./WindowsSdk/Sources/WindowsSdk/cg_Microsoft.*.swift ./WindowsApp/Sources/WindowsApp/
+rm ./WindowsApp/Sources/WindowsApp/generated/*.swift
+mv ./WindowsSdk/Sources/WindowsSdk/Microsoft.*.swift ./WindowsApp/Sources/WindowsApp/generated
+mv ./WindowsSdk/Sources/WindowsSdk/cg_Microsoft.*.swift ./WindowsApp/Sources/WindowsApp/generated
 
-rm ./Win2D/Sources/Win2D/*.swift
-mv ./WindowsApp/Sources/WindowsApp/*Graphics.Canvas* ./Win2D/Sources/Win2D/
+rm ./Win2D/Sources/Win2D/generated/*.swift
+mv ./WindowsApp/Sources/WindowsApp/*Graphics.Canvas* ./Win2D/Sources/Win2D/generated
 
